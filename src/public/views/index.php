a9c6ae161b6586ca9d2100c135c3b19fb8ba1c69
Joel Greek <joelgreek@gmail.com>
2018-05-17 13:09:00 +0200

    Css and navbar changes.
    Css and navbar changes.

--- src/public/views/index.php
+++ src/public/views/index.php
@@ -42,7 +42,7 @@
 </div>
     <?php else : ?>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -50,18 +50,15 @@
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" id="users" href="PleaseEnableJavascript.html"> Users <span class="sr-only">(current)</span> </a>
+        <a class="nav-link" id="users" href="PleaseEnableJavascript.html" onclick="getAllUsers();return false;"> Users <span class="sr-only">(current)</span> </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" id="entries" href="#">Entries</a>
+        <a class="nav-link" id="entries" href="PleaseEnableJavascript.html" onclick="getAllEntries();return false;">Entries</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" id="comments"  href="PleaseEnableJavascript.html" onclick="getAllComments();return false;">Comments</a>
+      </li>
 
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" id="" href="#">About</a>
-      </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search">
