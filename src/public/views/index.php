15c1aacddf9e128658e9a778e9f1c1b706bca520
Joel Greek <joelgreek@gmail.com>
2018-05-17 12:20:20 +0200

    Navbar update
    List test

--- src/public/views/index.php
+++ src/public/views/index.php
@@ -41,8 +41,36 @@
     </form>
 </div>
     <?php else : ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" id="users" href="PleaseEnableJavascript.html"> Users <span class="sr-only">(current)</span> </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="entries" href="#">Entries</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="comments"  href="PleaseEnableJavascript.html" onclick="getAllComments();return false;">Comments</a>
+
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
     <div class="container">
     <br>
     <h1>New Entry</h1>
@@ -55,24 +83,9 @@
       </div>
     </div>
 
-    <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
+
   
-<div class="container">
-  <h3>Basic Navbar Example</h3>
-  <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
-</div>
+
 
   
     
