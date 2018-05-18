003cf0060a01d0ea7cbaa54567452f27c882b10c
AndreasHansson1 <32000349+AndreasHansson1@users.noreply.github.com>
2018-05-18 09:57:38 +0200

    ..

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -6,30 +6,6 @@
 
 // main();
 
-// function getAllUsers() {
-//   fetch('api/users')
-
-//     .then(res => res.json())
-//     .then(users => {
-//       for (var key in users) {
-//       let userID = users[key].userID;
-//       let username = users[key].username; 
-//         if (users.hasOwnProperty(key)) {
-//          var output = '';
-
-//          output += '<h1>Users ' + '</h1>' +
-//            '<ul id="list">' +
-//            '<li><strong>User ID:</strong> ' + userID + '</li>' +
-//            '<li><strong>Username:</strong> ' + username + '</li>' +
-//            '</ul><br>';
-
-//          document.getElementById('container1').innerHTML = output;
-//         }
-//       }
-//     });
-// }
-// getAllUsers();
-
 function getAllUsers() {
   fetch('api/users')
 
@@ -60,7 +36,8 @@ function getAllUsers() {
 // getAllUsers();
 
 function getAllEntries() {
-  fetch('api/entries')
+  let quantity = document.getElementById('entryQuantity').value;
+  fetch('api/entries?limit=' + quantity)
 
     .then(res => res.json())
     .then(entries => {
@@ -68,7 +45,9 @@ function getAllEntries() {
       let h = document.createTextNode('Entries');
       header.appendChild(h);
       document.getElementById('container1').appendChild(header);
+
       for (var key in entries) {
+        
         let title = entries[key].title;
         let content = entries[key].content;
         let createdBy = entries[key].createdBy;
@@ -97,6 +76,7 @@ function getAllEntries() {
           // Create Delete Button
           let btn1 = document.createElement('button');
           btn1.class = "btn1"; // Set a class name
+          btn1.id = entries[key].entryID;
           btn1.setAttribute("onclick", deleteEntry);
           btn1.onclick = deleteEntry; // Calls function to delete entry
           document.getElementsByClassName("btn1").onclick = deleteEntry;
@@ -111,18 +91,19 @@ function getAllEntries() {
 
 
 function getAllComments() {
-  fetch('api/comments')
+  let quantity = document.getElementById('commentQuantity').value;
+  fetch('api/comments?limit=' + quantity)
     .then(res => res.json())
-    .then(entries => {
+    .then(comments => {
       // Create H1 headertext
       let header = document.createElement('h1');
       let h = document.createTextNode('Comments');
       header.appendChild(h);
       document.getElementById('container2').appendChild(header);
-      for (var key in entries) {
-        let content = entries[key].content;
-        let createdBy = entries[key].createdBy;
-        if (entries.hasOwnProperty(key)) {
+      for (var key in comments) {
+        let content = comments[key].content;
+        let createdBy = comments[key].createdBy;
+        if (comments.hasOwnProperty(key)) {
           let p1 = document.createElement('p');
           let p2 = document.createElement('p'); 
           let t1 = document.createTextNode('Content: ' + content);
@@ -134,6 +115,7 @@ function getAllComments() {
           // Create Edit Button
           let btn = document.createElement('button');
           let t = document.createTextNode('Edit'); 
+          
           btn.appendChild(t); 
           document.getElementById('container2').appendChild(btn);
           // Create Delete Button
@@ -184,7 +166,7 @@ function getOneComment() {
 }
 
 function deleteEntry() {
-  let ID = this.entryID;
+  let ID = this.id;
   alert(ID);
   fetch('api/entries/')
     .then(res => res.json())
