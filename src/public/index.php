97d71223827a04721a771c7ba649364a6451c119
AndreasHansson1 <32000349+AndreasHansson1@users.noreply.github.com>
2018-05-23 16:29:03 +0200

    todo stuff away

--- src/public/index.php
+++ src/public/index.php
@@ -77,23 +77,6 @@ $app->get('/logout', function ($request, $response, $args) {
  */
 $app->group('/api', function () use ($app) {
 
-    // GET http://localhost:XXXX/api/todos
-    $app->get('/todos', function ($request, $response, $args) {
-        /**
-         * $this->get('Todos') is available to us because we injected it into the container
-         * in 'App/container.php'. This makes it easier for us to call the database
-         * inside our routes.
-         */
-        $allTodos = $this->todos->getAll();
-        /**
-         * Wrapping the data when returning as a safety thing
-         * https://www.owasp.org/index.php/AJAX_Security_Cheat_Sheet#Server_Side
-         */
-        return $response->withJson(['data' => $allTodos]);
-    });
-
-
-
     // GET ALL USERS -- localhost:3000/api/users
     $app->get('/users', function ($request, $response, $args) {
         $allUsers = $this->users->getAll();
@@ -107,21 +90,6 @@ $app->group('/api', function () use ($app) {
         $allEntries = $this->entries->getAll($limit);
   
         return $response->withJson($allEntries);
-    });
-        
-
-    // GET http://localhost:XXXX/api/todos/5
-    $app->get('/todos/{id}', function ($request, $response, $args) {
-        /**
-         * {id} is a placeholder for whatever you write after todos. So if we write
-         * /todos/4 the {id} will be 4. This gets saved in the $args array
-         * $args['id'] === 4
-         * The name inside of '$args' must match the placeholder in the url
-         * https://www.slimframework.com/docs/v3/objects/router.html#route-placeholders
-         */
-        $id = $args['id'];
-        $singleTodo = $this->todos->getOne($id);
-        return $response->withJson(['data' => $singleTodo]);
     });
 
     // GET SINGLE USER
@@ -138,26 +106,9 @@ $app->group('/api', function () use ($app) {
         return $response->withJson($singleEntry);
     });
 
-
-    // POST http://localhost:XXXX/api/todos
-    $app->post('/todos', function ($request, $response, $args) {
-        /**
-         * Everything sent in 'body' when doing a POST-request can be
-         * extracted with 'getParsedBody()' from the request-object
-         * https://www.slimframework.com/docs/v3/objects/request.html#the-request-body
-         */
-        $body = $request->getParsedBody();
-        $newTodo = $this->todos->add($body);
-        return $response->withJson(['data' => $newTodo]);
-    });
-
     // POST ENTRY
     $app->post('/entries', function ($request, $response, $args) { 
-        /**
-         * Everything sent in 'body' when doing a POST-request can be
-         * extracted with 'getParsedBody()' from the request-object
-         * https://www.slimframework.com/docs/v3/objects/request.html#the-request-body
-         */
+       
         $body = $request->getParsedBody();
         $newEntry = $this->entries->add($body);
         return $response->withJson($newEntry);
@@ -210,8 +161,6 @@ $app->group('/api', function () use ($app) {
         $entryID = $args['entryID'];
         $title = $request->getParam('title');
         $content = $request->getParam('content'); 
-        // $createdBy = $request->getParam('createdBy');
-        // $createdAt = $request->getParam('createdAt'); 
         $body = $request->getParsedBody();
         $editEntry = $this->entries->editEntry($body, $entryID);
         return $response->withJson($editEntry);
@@ -224,27 +173,6 @@ $app->group('/api', function () use ($app) {
         $newComment = $this->entries->addComment($body);
         return $response->withJson($newComment);
     });
-
-    // LOGIN
-//     $app->post('/login', function ($request, $response, $args) {
-    
-//     $body = $request->getParsedBody();
-//     $fetchUserStatement = $this->db->prepare('SELECT * FROM users WHERE username = :username');
-//     $fetchUserStatement->execute([
-//         ':username' => $body['username']
-//     ]);
-//     $user = $fetchUserStatement->fetch();
-//     if (password_verify($body['password'], $user['password'])) {
-//         if(isset($_POST["username"]) && $_POST["password"]!=""){
-//             } else {
-//                 echo 'No empty fields allowed!';
-//             }
-//         $_SESSION['loggedIn'] = true;
-//         $_SESSION['userID'] = $user['id'];
-//         return $response->withJson(['data' => [ $user['id'], $user['username'] ]]);
-//     }
-//     return $response->withJson(['error' => 'wrong password']);
-// });
 
     // GET ALL ENTRIES FROM A USER
     $app->get('/users/{id}/entries', function ($request, $response, $args) {
